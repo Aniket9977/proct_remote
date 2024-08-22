17e66e85528c1c5d77a8ccff0eabcc839c549f95
Aniket9977 <aniketvishwakarma459@gmail.com>
2024-08-22 12:07:38 +0530

    predictor loc updated

--- face_detection/detector.py
+++ face_detection/detector.py
@@ -2,5 +2,5 @@ import dlib
 
 def get_face_detector():
     detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor("shape_model\shape_predictor_68_face_landmarks.dat")
+    predictor = dlib.shape_predictor("shape_model/shape_predictor_68_face_landmarks.dat")
     return detector, predictor
